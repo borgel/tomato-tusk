414bd5d8b4a496f083d752faa88dc29209988e92
borgel <iborgel@gmail.com>
2017-01-15 11:25:02 -0800

    Rearrange pins, UART works

--- src/Tomato Tusk/Tomato Tusk.cydsn/main.c
+++ src/Tomato Tusk/Tomato Tusk.cydsn/main.c
@@ -3,6 +3,11 @@
 #include <stdbool.h>
 int main()
 {
+    CyGlobalIntEnable;
+    
+    // init uart
+    UART_Start();
+    UART_PutString("Tomato Tusk [__DATE__] Starting...\n");
 
     for(;;)
     {
